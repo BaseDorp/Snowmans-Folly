3a7826cd14a41da7fdbabcd109f489f8ab586d34
JSchoppe <jethro.schoppenhorst@gmail.com>
2021-01-22 02:04:09 -0600

    adds utility functions, mono update hijacker
    adds utility extension functions as well as importing a script that exposes a singleton MonoBehaviour where non-mono classes can latch onto the update loop

--- Assets/Scripts/Utilities/Extensions/GenericCollectionExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/GenericCollectionExtensions.cs
@@ -1,10 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// Provides utility extension methods for collections.
+/// </summary>
 public static class GenericCollectionExtensions
 {
+    #region IList Extensions
+    /// <summary>
+    /// Retrieves a random element from this collection.
+    /// </summary>
+    /// <param name="collection">The collection to pull from.</param>
+    /// <returns>Any element from the collection base on an evenly distributed random function.</returns>
     public static T RandomElement<T>(this IList<T> collection)
     {
         return collection[Random.Range(0, collection.Count)];
     }
+    #endregion
+    #region Array Extensions
+    /// <summary>
+    /// Returns a copy of the array with an index of the array removed.
+    /// </summary>
+    /// <param name="collection">The array to remove from.</param>
+    /// <param name="indexToRemove">The index to remove from the array.</param>
+    /// <returns>A new array without the specified index.</returns>
+    public static T[] WithIndexRemoved<T>(this T[] collection, int indexToRemove)
+    {
+        T[] newCollection = new T[collection.Length - 1];
+        int retrieverIndex = 0;
+        for (int i = 0; i < newCollection.Length; i++)
+        {
+            // If the removed element has been hit,
+            // have the retriever index step over it.
+            if (i == indexToRemove)
+                retrieverIndex++;
+            newCollection[i] = collection[retrieverIndex];
+            // Increment retriever index.
+            retrieverIndex++;
+        }
+        return newCollection;
+    }
+    #endregion
 }
---
+++ Assets/Scripts/Utilities/Extensions/IntExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Provides utility extension methods for integers.
+/// </summary>
+public static class IntExtensions
+{
+    /// <summary>
+    /// Clamps an integer value in place into the given range.
+    /// </summary>
+    /// <param name="value">The value to clamp.</param>
+    /// <param name="min">The minimum value for the int.</param>
+    /// <param name="max">The maximum value for the int.</param>
+    public static void Clamp(this ref int value, int min, int max)
+    {
+        value = Mathf.Clamp(value, min, max);
+    }
+}
