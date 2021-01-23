d30ce6b5799419376067eebb55a0cf37477cb767
Ian Jeannin <ian.jeannin@loop.colum.edu>
2021-01-23 02:59:12 -0500

    Add interactable spawning.

---
+++ Assets/Scripts/Interactables/Hazard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : Interactable
+{
+    [Header("Hazard Parameters")]
+    [Tooltip("How much speed the player loses when hitting this hazard.")]
+    [SerializeField]
+    private float velocityLoss;
+
+    public override void OnPlayerEnter(SnowmanControl player)
+    {
+        player.GetComponent<Rigidbody2D>().AddForce(-player.gameObject.transform.right*velocityLoss);
+        /*float x = player.GetComponent<Rigidbody2D>().velocity.x;
+        float y = player.GetComponent<Rigidbody2D>().velocity.y;
+        x = velocityLoss;
+        y = velocityLoss;
+        player.GetComponent<Rigidbody2D>().velocity.Set(x, y);*/
+    }
+}
