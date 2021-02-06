2ca4ec4e2b94d2465b7933ad9a2c3a5d3d4fb2da
Ian Jeannin <ian.jeannin@loop.colum.edu>
2021-02-06 12:04:14 -0500

    Add stamina gain powerup

---
+++ Assets/Scripts/Interactables/PowerUps/StaminaBoost.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaBoost : Interactable
+{
+    [Header("Stamina Parameter")]
+    [Tooltip("How much Stamina is added when the player hits this interactable.")]
+    [SerializeField]
+    private float staminaFlatGain;
+    [Tooltip("The percent stamina that is added when the player hits this interactable.")]
+    [SerializeField]
+    [Range(0,100)]
+    private float staminaPercentGain;
+    [Tooltip("If true, will use percent gain")]
+    [SerializeField]
+    private bool usePercentGain;
+
+    public override void OnPlayerEnter(SnowmanControl player)
+    {
+        if(usePercentGain)
+        {
+            player.GetComponent<StaminaSystem>().Stamina += player.GetComponent<StaminaSystem>().MaxStamina*(staminaPercentGain/100);
+        }
+        else
+        {
+            player.GetComponent<StaminaSystem>().Stamina += staminaFlatGain;
+        }
+        Destroy(gameObject);
+    }
+}
