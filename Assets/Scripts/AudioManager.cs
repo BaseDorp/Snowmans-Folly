ac75573865de98ddafc32cf9efa8349342ca886b
Ian Jeannin <ian.jeannin@loop.colum.edu>
2021-02-12 10:34:01 -0500

    Add third bgm track. Add script for managing audio, using audio mixer groups

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
@@ -11,14 +12,29 @@ public class AudioManager : MonoBehaviour
     [SerializeField]
     private AudioClip[] collisionClips;
     [SerializeField]
-    private AudioClip powerupClips;
+    private AudioClip[] powerupClips;
     [SerializeField]
-    private AudioClip slidingClips;
+    private AudioClip[] slidingClips;
+    [SerializeField]
+    private AudioClip menuBGMClip;
     [SerializeField]
     private AudioClip[] bgmClips;
+    [SerializeField]
+    private AudioClip cookieClip;
+    [SerializeField]
+    private AudioMixerGroup sfxMixer;
+    [SerializeField]
+    private AudioMixerGroup bgmMixer;
 
     private bool sfxMuted = false;
     private bool bgmMuted = false;
+
+    private void Start()
+    {
+        sfxSource.outputAudioMixerGroup = sfxMixer;
+        bgmSource.outputAudioMixerGroup = bgmMixer;
+        PlayBGM(menuBGMClip);
+    }
 
     public void PlaySFX(AudioClip SFX)
     {
@@ -29,6 +45,35 @@ public class AudioManager : MonoBehaviour
     {
         bgmSource.clip = BGM;
         bgmSource.Play();
+    }
+
+    public void PlayCookie()
+    {
+        PlaySFX(cookieClip);
+    }
+
+    public void PlayPowerup()
+    {
+        int x = Random.Range(0, powerupClips.Length);
+        PlaySFX(powerupClips[x]);
+    }
+
+    public void PlayCollision()
+    {
+        int x = Random.Range(0, collisionClips.Length);
+        PlaySFX(collisionClips[x]);
+    }
+    
+    public void PlaySlide()
+    {
+        int x = Random.Range(0, slidingClips.Length);
+        PlaySFX(slidingClips[x]);
+    }
+
+    public void PlayRandomBGM()
+    {
+        int x = Random.Range(0, bgmClips.Length);
+        PlayBGM(bgmClips[x]);
     }
 
 }
